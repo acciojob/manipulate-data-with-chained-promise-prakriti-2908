986644346fce358368b44539e7326bcf806cc2e7
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-03-29 00:26:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+let output = document.getElementById("output");
+
+let arr = [1,2,3,4];
+let a;
+let p1 = new Promise((resolve)=>{
+	setTimeout(()=>{
+		a = arr.filter(v=>v%2==0);
+		resolve(a);
+	},1000);
+});
+
+p1.then((data)=>{
+	output.innerHTML = data.join(" ");
+	return new Promise((resolve)=>{
+	let b = a.map(v=>v*2);
+		setTimeout(()=>{
+			resolve(b);
+		})
+	})
+}).then((data)=>{
+	output.innerHTML = data.join(" ");
+})
